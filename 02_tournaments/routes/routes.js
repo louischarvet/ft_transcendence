9ebc25c6485fa3e6a69287c49ed8eee8e2c55228
Nathan <trijaunathan@gmail.com>
2025-07-18 16:51:34 +0200

    dokcer run ok but not really good now

--- 02_tournaments/routes/routes.js
+++ 02_tournaments/routes/routes.js
@@ -1,3 +1,5 @@
+
+
 async function routesPlugin(fastify, options) {
 	fastify.get('/data', async (request, reply) => {
 		return { message: "Hello from auth", test: { testA: "Blob", testB: "Blub" } };
