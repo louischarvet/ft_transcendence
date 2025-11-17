f55057455585a1152028fa478bdc4e026b633895
Nathan Trijaud <natrijau@1C5.42angouleme.fr>
2025-11-17 15:02:05 +0100

    merge branch baptiste

--- frontend/dist/pages/Tournament.js
+++
@@ -1,6 +0,0 @@
-export default function Tournament() {
-    const h1 = document.createElement('h1');
-    h1.className = 'text-xl font-bold';
-    h1.textContent = 'Tournament page';
-    return h1;
-}
